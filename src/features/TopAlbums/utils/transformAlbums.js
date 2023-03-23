d786cfcb9742e81473b54839cbd223aa097e7f71
Andrew Yi <andrewjyi@gmail.com>
2023-03-23 10:03:36 -0700

    renamed variable

--- src/features/TopAlbums/utils/transformAlbums.js
+++ src/features/TopAlbums/utils/transformAlbums.js
@@ -1,17 +1,17 @@
 import { getMuricanDateFormat } from "../../../utils/utils";
 
-const transformAlbums = (data) => {
-  return data.map((data) => ({
-    id: data["id"]["attributes"]["im:id"],
+const transformAlbums = (albums) => {
+  return albums.map((album) => ({
+    id: album["id"]["attributes"]["im:id"],
     imgClass: "h-72 w-72 rounded",
-    imgMeta: data["im:name"]["label"],
-    imgSrc: data["im:image"][2]["label"],
-    name: data["im:name"]["label"],
-    artist: data["im:artist"]["label"],
-    itemCount: data["im:itemCount"]["label"],
-    price: data["im:price"]["label"],
-    releaseDate: getMuricanDateFormat(data["im:releaseDate"]["label"]),
-    rights: data["rights"]["label"],
+    imgMeta: album["im:name"]["label"],
+    imgSrc: album["im:image"][2]["label"],
+    name: album["im:name"]["label"],
+    artist: album["im:artist"]["label"],
+    itemCount: album["im:itemCount"]["label"],
+    price: album["im:price"]["label"],
+    releaseDate: getMuricanDateFormat(album["im:releaseDate"]["label"]),
+    rights: album["rights"]["label"],
   }));
 };
 
